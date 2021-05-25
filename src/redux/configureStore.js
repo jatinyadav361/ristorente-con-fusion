2b5f9a40d5469f55c93dde6b9ad0db08dfc2bd7b
jatinyadav361 <jatinrao361@gmail.com>
2021-05-25 17:53:47 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,20 @@
-import { Reducer, initialState } from './reducer';
-import { createStore } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import { createStore, combineReducers } from 'redux';
 
 // Configuring react store
 export const configureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        // We have splitted the reducer into multiple reducers that manage partial state, and then 
+        // combined them together using combineReducers().
+        combineReducers({
+            dishes : Dishes,
+            comments : Comments,
+            leaders : Leaders,
+            promotions : Promotions
+        })
     );
     return store;
 }
